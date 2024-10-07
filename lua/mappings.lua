a91a756d3619d1245aad0eb2048c685166817c13
haxorg-ux <josheechevarrieta@gmail.com>
2024-10-07 13:09:37 -0300

    up mappings menu, minty, showkeys

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -25,4 +25,24 @@ vim.keymap.set("n", "<RightMouse>", function()
   require("menu").open(options, { mouse = true })
 end, {})
 
+-- Keyboard users
+vim.keymap.set("n", "<C-t>", function()
+    require("menu").open("default")
+end, {})
+
+-- mouse users + nvimtree users!
+vim.keymap.set("n", "<RightMouse>", function()
+    vim.cmd.exec('"normal! \\<RightMouse>"')
+
+    local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
+    require("menu").open(options, { mouse = true })
+end, {})
+
+map("n", "<C-m>", function()
+    require("minty.huefy").open()
+end)
+
+map("n", "<C-k>", function()
+    require("showkeys").toggle()
+end)
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
